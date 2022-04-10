9f0b4f04e3239aa988aeb8c0b706fb50efca76a2
noir <90619153+noirdevelopment@users.noreply.github.com>
2022-04-10 09:54:28 -0500

    Update searcher.js

--- script/searcher.js
+++ script/searcher.js
@@ -5,4 +5,4 @@ function encode(str) {
 function searcher() {
   var input = document.getElementById("searchBox").value;
   if (parseInt(input) === null) return;
-  var urlObj = new window.URL(window.location.href); var url = "https://cdn.bobux.gq/service/" + encode("https://" + input); if (url) { var win;{ if (win) { win.focus(); } else { win = window.location.href(); win.document.body.style.margin = '0'; win.document.body.style.height = '100vh'; var iframe = win.document.createElement('iframe'); iframe.style.border = 'none'; iframe.style.width = '100%'; iframe.style.height = '100%'; iframe.style.margin = '0'; iframe.src = url; win.document.body.appendChild(iframe)}}}}
+  var urlObj = new window.URL(window.location.href); var url = "https://cdn.bobux.gq/service/" + encode("https://" + input); if (url) { var win;{ if (win) { win.focus(); } else { win = window.open(); win.document.body.style.margin = '0'; win.document.body.style.height = '100vh'; var iframe = win.document.createElement('iframe'); iframe.style.border = 'none'; iframe.style.width = '100%'; iframe.style.height = '100%'; iframe.style.margin = '0'; iframe.src = url; win.document.body.appendChild(iframe)}}}}
